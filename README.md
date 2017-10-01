2986c449f42994790e0d6fec8a6a9fc3a175ecf0
Eric Nograles <grales@gmail.com>
2017-10-01 08:46:12 -0400

    initial scaffold

---
+++ README.md
@@ -0,0 +1,23 @@
+# GraphQL of Thrones
+
+
+
+This project was created with [Apollo Launchpad](https://launchpad.graphql.com)
+
+You can see the original pad at [https://launchpad.graphql.com/50l9p0399](https://launchpad.graphql.com/50l9p0399)
+
+### Quick start guide
+
+```bash
+npm install
+
+npm start
+```
+
+
+
+
+
+Happy hacking!
+
+The Apollo team :)
---
+++ src/schema.js
@@ -0,0 +1,112 @@
+// Welcome to Launchpad!
+// Log in to edit and save pads, run queries in GraphiQL on the right.
+// Click "Download" above to get a zip with a standalone Node.js server.
+// See docs and examples at https://github.com/apollographql/awesome-launchpad
+
+// graphql-tools combines a schema string with resolvers.
+import { makeExecutableSchema } from 'graphql-tools';
+
+const users = [
+  { id: 1, first_name: 'Tywin', last_name: 'Lannister', email_address: 'tywin.lannister@casterlyrock.com'},
+  { id: 2, first_name: 'Cersei', last_name: 'Lannister', email_address: 'cersei.lannister@casterlyrock.com'},
+  { id: 3, first_name: 'Jaime', last_name: 'Lannister', email_address: 'jaime.lannister@casterlyrock.com'},
+  { id: 4, first_name: 'Tyrion', last_name: 'Lannister', email_address: 'tyrion.lannister@casterlyrock.com'},
+];
+
+const friends = [
+  { id: 1, friend_id: 2 },
+  { id: 1, friend_id: 3 },
+  { id: 1, friend_id: 2 },
+  { id: 4, friend_id: 3 }
+];
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = `
+  type Query {
+		# Users in the **GraphQL of Thrones**
+    users(
+			# (Array) The user's ID in GraphQL of Thrones
+			id: [Int!]
+		) : [User!]
+  }
+
+  type Mutation {
+		# Login to **GraphQL of Thrones**
+    login(email: String!, password: String!) : User
+  }
+	
+	# The User type
+	type User {
+		first_name: String
+		last_name: String
+		friends: [User!]
+		
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    users: (root, args, context) => {
+      let results = users
+      	.filter(user => {
+        	return args.id.indexOf(user.id) > -1;
+      	})
+      	.map(user => {
+          let friendIds = friends
+          	.filter(friendRecord => {
+              return friendRecord.id === user.id;
+            })
+          	.map(friendRecord => {
+              return friendRecord.friend_id;
+            });
+          
+          user.friends = users
+            .filter(existingUser => {
+            	return friendIds.indexOf(existingUser.id) > -1;
+          	});
+          
+          return user;
+        });
+      
+      return results;
+    }
+  },
+  Mutation: {
+    login: (root, args, context) => {
+      return users
+      	.find(user => {
+          user.email_address === args.email;
+        });
+    }
+  }
+};
+
+// Required: Export the GraphQL.js schema object as "schema"
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+// Optional: Export a function to get context from the request. It accepts two
+// parameters - headers (lowercased http headers) and secrets (secrets defined
+// in secrets section). It must return an object (or a promise resolving to it).
+export function context(headers, secrets) {
+  return {
+    headers,
+    secrets,
+  };
+};
+
+// Optional: Export a root value to be passed during execution
+// export const rootValue = {};
+
+// Optional: Export a root function, that returns root to be passed
+// during execution, accepting headers and secrets. It can return a
+// promise. rootFunction takes precedence over rootValue.
+// export function rootFunction(headers, secrets) {
+//   return {
+//     headers,
+//     secrets,
+//   };
+// };
